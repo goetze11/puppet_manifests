3b6cb5dab37d95a0cb39d43f6c2e45ede84d102a
root <root@avm3.llnl.gov>
2017-07-20 13:44:09 -0700

    Added files

---
+++ projectname.pp
@@ -0,0 +1,10 @@
+node "avm3.llnl.gov" {
+
+file { '/root/example_file.txt':
+    ensure => "file",
+    owner  => "root",
+    group  => "root",
+    mode   => "700",
+    content => "Congratulations! Puppet has created this file.",}
+
+}
